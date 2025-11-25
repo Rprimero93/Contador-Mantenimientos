4e0e06f96653c6dabedbcf6925da60955c59f90d
Rprimero <villamil76@gmail.com>
2025-11-25 10:20:53 -0500

    Actualizacion25-11-2025

--- Contador/JS.js
+++ Contador/JS.js
@@ -1,4 +1,4 @@
-const numeroFinal = 1086;
+const numeroFinal = 1241;
 const elementoContador = document.getElementById('contador');
 const mensajeFinal = document.getElementById('mensajeFinal');
 const duracionTotal = 3000; // 3 segundos
